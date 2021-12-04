238190db5ec0f2f63277432d2961ca00319dcbec
dstarr <david@elegantcode.com>
2021-12-04 10:40:24 -0600

    removed security measures in the code

--- saas/labs/lab-code/end/SaaSFunctions/Webhook.cs
+++ saas/labs/lab-code/end/SaaSFunctions/Webhook.cs
@@ -9,9 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
-using System.Net;
 
-namespace SaasFunctions
+namespace SaaSFunctions
 {
     public static class Webhook
     {
@@ -39,54 +38,14 @@ namespace SaasFunctions
 
             PrintToLogHeader();
 
-            if (!RequestIsSecure())
-            {
-                _logger.LogInformation("Security checks did not pass!");
-                return new StatusCodeResult((int)HttpStatusCode.Forbidden);
-            }
-
             var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-
-            if (!SubscriptionEventIsValid(data))
-            {
-                _logger.LogInformation("Event informtion was invalid!");
-                return new StatusCodeResult((int)HttpStatusCode.Conflict);
-            }
 
             PrintToLogPayload(data);
 
             return new OkResult();
         }
 
-        /// <summary>
-        /// Calls functions that check various security aspects of this webhook
-        /// </summary>
-        /// <returns>bool indicating if the request is secure</returns>
-        private static bool RequestIsSecure()
-        {
-            // set up the configuration
-            var config = new ConfigurationBuilder()
-                .SetBasePath(_executionContext.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-
-            if (!QueryStringIsValid(config))
-            {
-                _logger.LogInformation("Querystring is invalid.");
-                return false;
-            }
-
-            if (!ClaimsAreValid(config))
-            {
-                _logger.LogInformation("Claims are invalid.");
-                return false;
-            }
-
-            return true;
-
-        }
 
         /// <summary>
         /// Checks that JWT claims are valid
@@ -134,64 +93,6 @@ namespace SaasFunctions
             {
                 _logger.LogInformation("Issuer does not match.");
                 return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Checks that the secret on the querystring is present and correct
-        /// </summary>
-        /// <param name="config">IConfigurationRoot</param>
-        /// <returns>A bool indicating if the querystring is valid.</returns>
-        private static bool QueryStringIsValid(IConfigurationRoot config)
-        {
-            // get the env:Variables
-            var secretKey = config["QueryStringSecret:SecretKey"];
-            var secretValue = config["QueryStringSecret:SecretValue"];
-
-            // ensure the key/value exists
-            if (_request.Query[secretKey].Count == 0)
-            {
-                _logger.LogInformation("No secret key on query string.");
-                _logger.LogInformation($"Expected: {secretKey}");
-                return false;
-            }
-
-            // look for a match on the SecretValue
-            var token = _request.Query[secretKey][0];
-            if (secretValue != token)
-            {
-                _logger.LogInformation("Wrong secret value on query string.");
-                _logger.LogInformation($"Expected: {secretValue}");
-
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Check the event actully occured
-        /// </summary>
-        /// <param name="data">the JSON paylod s a dynamic value</param>
-        /// <returns>Whether of the the reported event is a valid one</returns>
-        private static bool SubscriptionEventIsValid(dynamic data)
-        {
-            switch (data.action)
-            {
-                case "Unsubscribed":
-                    // 1. Use the SaaS Fulfillment API to fetch the relevant subscription
-                    // 2. Check the subscription's "saasSubscriptionStatus": "Unsubscribed"
-                    // 3. Return false on unexpected result
-                    break;
-
-                case "ChangePlan":
-                    // 1. Use the SaaS Operation API to validate a ChangePlan has been requested
-                    // 2. return false on unexpected result
-                    break;
-
-                // etc.
             }
 
             return true;
